27ec7809ced31ee75936eabed177e94ed5b6a72c
weijerj6 <julweijers@gmail.com>
2022-11-08 13:17:03 +0100

    Insteek project veranderd. Introductie veranderd.

--- Code versie1/GymMaatjeV1.java
+++ Code versie1/GymMaatjeV1.java
@@ -1,4 +1,5 @@
 // @author: Julian Weijers
+// Versie 1: ruwe schets
 // Doel: Doe berekeningen met het aantal gewicht wat de gebruiker kan tillen en geef hierover resultaat
 
 import java.util.Scanner;
@@ -8,27 +9,39 @@ public class GymMaatjeV1 {
         // Scanner
         Scanner input = new Scanner(System.in);
 
-        // Variabelen
-        int eenheidKeuze = 0;
+        // Variabelen en constanten
 
-        // Introductie
-        System.out.println("Kies uw taal / Choose your language");
-        System.out.print("Voer 1 in voor Nederlands / Enter 2 for English: ");
-        int taalKeuze = input.nextInt(); // Mag later verwijderd worden, was alleen voor checken
+        // Introductie met vragen gebruiker
+        System.out.println("Welkom bij GymMaatje! Jouw persoonlijke maatje voor in de gym!");
+        System.out.print("Allereerst, wat is je naam? ");
+        String naamGebruiker = input.nextLine(); // slaat gebruikers naam op
 
-        // Zorg voor juist input
+        System.out.print("Wat is je lengte in centimeter? ");
+        int lengteGebruiker = input.nextInt(); // slaat gebruikers lengte op
+
+        System.out.print("Hoeveel weeg je in kilogram (1 decimaal)? ");
+        double gewichtGebruiker = input.nextDouble();
+
+        System.out.print("Wil je de berekeningen in kilogram (toets 1) of in ponden (toets 2) doen? ");
+        int eenheidKeuze = input.nextInt(); // Keuze voor gebruiker om te rekenen met ponden
+
+
+
+
+
+        /* Later doen : zorg voor correcte user input
         while (taalKeuze < 1 || taalKeuze > 2){
             System.out.println("\tOnjuiste invoer / Wrong input");
             System.out.print("\tVoer 1 in voor Nederlands / Enter 2 for English: ");
             taalKeuze = input.nextInt();
-        }
+        } */
 
         // User input eenheid gewicht
-        switch (taalKeuze){
+       /* switch (taalKeuze){
             case 1:
                 System.out.println("\nWil je rekenen met kilogram (kg) of met pond (lbs)?");
                 System.out.print("Voor kilogram (kg) toets 1 / voor pond (lbs) toets 2: ");
-                eenheidKeuze = input.nextInt();
+                //eenheidKeuze = input.nextInt();
 
                 while (eenheidKeuze < 1 || eenheidKeuze > 2) {
                     System.out.println("\tOnjuiste invoer, probeer opnieuw.");
@@ -47,7 +60,7 @@ public class GymMaatjeV1 {
                     eenheidKeuze = input.nextInt();
                 }
                 break;
-        }
+        } */
 
 
     }
